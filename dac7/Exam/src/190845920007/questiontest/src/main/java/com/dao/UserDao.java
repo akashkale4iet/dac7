034fdb7be4b7c5134f3af28afc21e206531a2ad4
akashkale4iet <akashkumarkale4iet@gmail.com>
2019-12-03 17:29:33 +0530

    yoofsdfo

--- dac7/Exam/src/190845920007/questiontest/src/main/java/com/dao/UserDao.java
+++
@@ -1,7 +0,0 @@
-package com.dao;
-
-public interface UserDao {
-
-	int insertUser(String fname, String lname, String cno, String email, String bdate, String city);
-
-}
--- dac7/Exam/src/190845920007/questiontest/src/main/java/com/service/UserInterface.java
+++
@@ -1,7 +0,0 @@
-package com.service;
-
-public interface UserInterface {
-
-	int insert(String fname, String lname, String cno, String email, String bdate, String city);
-
-}
